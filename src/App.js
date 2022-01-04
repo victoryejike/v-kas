19b3940acb7c1aa7cee53a69554a4fc325bccde1
Ejike Victory <victoryejike@gmail.com>
2022-01-04 01:34:20 +0100

    structure of files and folders done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react'
 import "./styles.css";
+
+import { Header, Footer, Blog, Features, Possibility, WhatGPT3 } from './containers';
+import { Brand, CTA, Navbar, } from './components';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/article/Article.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Article.css';
+
+export const Article = () => {
+  return (
+    <div>
+      Article
+    </div>
+  )
+}
+
+export default Article;
