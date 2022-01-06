3f2a8f1fb4932d3b820a87b92967e507d12b3e66
Ejike Victory <victoryejike@gmail.com>
2022-01-06 20:19:06 +0100

    reonitialised the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import "./styles.css";
+import logo from './logo.svg';
+import './App.css';
 
-import { Header, Footer, Blog, Features, Possibility, WhatGPT3 } from './containers';
-import { Brand, CTA, Navbar, } from './components';
-
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <div className='gradient__bg'>
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
