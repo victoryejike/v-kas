7933ce510ec17d10edf188169000d51d58311219
Ejike Victory <victoryejike@gmail.com>
2022-01-09 23:57:43 +0100

    Features section completed

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -8,8 +8,8 @@ const About = () => {
       <div className='about__description'>
         <Feature title='What is GPT-3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.' />
       </div>
-      <div className='about__caption gradient__text'>
-        <h2>The possibilities are beyond your imagination</h2>
+      <div className='about__caption'>
+        <h2 className='gradient__text'>The possibilities are beyond your imagination</h2>
         <p>Explore The Library</p>
       </div>
       <div className='about__offers'>
--- src/containers/features/Features.js
+++ src/containers/features/Features.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './features.css'
+import { Feature } from '../../components'
 
 const Features = () => {
   return (
-    <div>
-      Features
-    </div>
+    <section className='features section__padding'>
+      <div className='features__heading'>
+        <h3 className='gradient__text'>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h3>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className='features__list'>
+        <Feature title='Improving end distrusts instantly' text='From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.' />
+        <Feature title='Become the tended active' text='Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.' />
+        <Feature title='Message or am nothing' text='Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.'/>
+        <Feature title='Really boy law county' text='Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.' />
+      </div>
+    </section>
   )
 }
 
